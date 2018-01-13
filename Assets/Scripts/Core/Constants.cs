354039afa284824ba07b15f6a37bebe3a6c60508
Piet Voute <pvoute@sfu.ca>
2018-01-13 12:18:42 -0800

    Set up the unity project and provide some basic implementation. Includes a character movement script, and a Singleton base class implementation. Also provides a tilemap sprite sheet which was aquired under a CC license, but we still can't use it for any submissions.

---
+++ Assets/Scripts/Core/Constants.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class Constants {
+	public static readonly string[] MOVEMENT_BLOCKING_LAYERS = {"Default", "Walls"};
+}
