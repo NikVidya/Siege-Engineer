e709503c19d9415f526c1696f958547beb305123
Unknown <n.mason1@outlook.com>
2018-02-22 17:40:47 -0800

    began work getting health bar to stick to camera when outside screen

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -7,22 +7,51 @@ public class HealthBar : MonoBehaviour
     public Damageable damageable;
     [SerializeField]
     private Gradient gradient;
-
     // private
     private float maxWidth;
     private SpriteRenderer spriteRenderer;
+    private Camera camera;
+    private Vector2 screenPos;
+    private bool movingWithCamera = false;
+    private Vector2 defaultPos;
     void Start()
     {
         maxWidth = transform.localScale.x;
         spriteRenderer = GetComponent<SpriteRenderer>();
+        camera = Camera.main;
+        defaultPos = new Vector2(transform.position.x, transform.position.y);
+
     }
     void Update()
     {
+        screenPos = camera.WorldToScreenPoint(transform.position);
+        if ((screenPos.y > Screen.height || screenPos.y < 0 || screenPos.x > Screen.width || screenPos.x < 0) && !movingWithCamera)
+        {
+            MoveToCamera();
+        }
+        else
+        {
+            MoveToDefault();
+        }
+        if (movingWithCamera)
+        {
+        }
     }
     public void changeWidth()
     {
-        // transform.localScale.Set(maxWidth * damageable.healthPercent, transform.localScale.y, transform.localScale.z);
         transform.localScale = new Vector3(maxWidth * damageable.healthPercent, transform.localScale.y, transform.localScale.z);
         spriteRenderer.color = gradient.Evaluate(damageable.healthPercent);
+    }
+
+    // Attach the healthbar to the camera
+    void MoveToCamera()
+    {
+        movingWithCamera = true;
+        transform.SetParent(camera.transform);
+    }
+    void MoveToDefault()
+    {
+        movingWithCamera = false;
+        transform.position = defaultPos;
     }
 }
