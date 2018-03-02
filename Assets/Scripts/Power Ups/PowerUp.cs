68f84b0adbf0217497777b7210bee0465e6132a4
Shawn Dhillon <shawnd@sfu.ca>
2018-03-01 17:10:38 -0800

    minor fix

--- Assets/Scripts/Power Ups/PowerUp.cs
+++ Assets/Scripts/Power Ups/PowerUp.cs
@@ -1,43 +1,7 @@
-﻿/*
- * Copyright (c) 2017 Razeware LLC
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
- * distribute, sublicense, create a derivative work, and/or sell copies of the 
- * Software in any work that is designed, intended, or marketed for pedagogical or 
- * instructional purposes related to programming, coding, application development, 
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works, 
- * or sale is expressly withheld.
- *    
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+﻿
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-/// <summary>
-/// Game independent Power Up logic supporting 2D and 3D modes.
-/// When collected, a Power Up has visuals switched off, but the Power Up gameobject exists until it is time for it to expire
-/// Subclasses of this must:
-/// 1. Implement PowerUpPayload()
-/// 2. Optionally Implement PowerUpHasExpired() to remove what was given in the payload
-/// 3. Call PowerUpHasExpired() when the power up has expired or tick ExpiresImmediately in inspector
-/// </summary>
 public class PowerUp : MonoBehaviour
 {
     public string powerUpName;
