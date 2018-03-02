0dad190e3b2c13296b8c0384357922dcf9f29dcf
Unknown <n.mason1@outlook.com>
2018-03-01 18:13:30 -0800

    added health bar prefab

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-// TODO make coloour of health bar change based on health (gradient)
+// TODO add outline/background to health bar to indicate not only max health but also damage rate
 public class HealthBar : MonoBehaviour
 {
     // public
@@ -10,7 +10,6 @@ public class HealthBar : MonoBehaviour
     // private
     private float maxWidth;
     private SpriteRenderer spriteRenderer;
-    private Camera camera;
     private Vector2 screenPos;
     private bool movingWithCamera = false;
     private Vector2 defaultPos;
@@ -18,24 +17,11 @@ public class HealthBar : MonoBehaviour
     {
         maxWidth = transform.localScale.x;
         spriteRenderer = GetComponent<SpriteRenderer>();
-        camera = Camera.main;
-        defaultPos = new Vector2(transform.position.x, transform.position.y);
 
     }
     void Update()
     {
-        screenPos = camera.WorldToScreenPoint(transform.position);
-        if ((screenPos.y > Screen.height || screenPos.y < 0 || screenPos.x > Screen.width || screenPos.x < 0) && !movingWithCamera)
-        {
-            MoveToCamera();
-        }
-        else
-        {
-            MoveToDefault();
-        }
-        if (movingWithCamera)
-        {
-        }
+
     }
     public void changeWidth()
     {
@@ -43,15 +29,4 @@ public class HealthBar : MonoBehaviour
         spriteRenderer.color = gradient.Evaluate(damageable.healthPercent);
     }
 
-    // Attach the healthbar to the camera
-    void MoveToCamera()
-    {
-        movingWithCamera = true;
-        transform.SetParent(camera.transform);
-    }
-    void MoveToDefault()
-    {
-        movingWithCamera = false;
-        transform.position = defaultPos;
-    }
 }
