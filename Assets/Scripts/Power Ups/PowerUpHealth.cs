8aa57ab8887567db04ec0a1056d404e582fa960c
Shawn Dhillon <shawnd@sfu.ca>
2018-03-01 16:59:42 -0800

    Started player upgrades implematation

---
+++ Assets/Scripts/Power Ups/PowerUpHealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpHealth : PowerUp
+{
+
+	public int healthBonus = 20;
+
+	protected override void PowerUpPayload()  // Checklist item 1
+	{
+		base.PowerUpPayload();
+
+		// Payload is to give some health bonus
+		player.SetHealthAdjustment(healthBonus);      
+	}
+}
+}
+
